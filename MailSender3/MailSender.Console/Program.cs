0a6ee7f79674ce0dd5f06403dbccef6668d490c1
Vedyakov11 <vedyakov11@gmail.com>
2019-04-03 20:07:59 +0300

    Создали консольное приложение без визуала.

--- MailSender3/MailSender.Console/Program.cs
+++ MailSender3/MailSender.Console/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace MailSender.Console
 {
@@ -10,6 +12,15 @@ namespace MailSender.Console
     {
         static void Main(string[] args)
         {
+            var message = new MailMessage("user@yandex.ru", "user@gmail.com");
+            message.Subject = "Тестовое письмо";
+            message.Body = $"Текст письма от {DateTime.Now}";
+
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("use_name", "password");
+
+            client.Send(message);
         }
     }
 }
