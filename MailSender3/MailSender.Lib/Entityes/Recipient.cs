b47fbe733c36c8d1e29d9e36d1ff6410c3e9d2e6
Vedyakov11 <vedyakov11@gmail.com>
2019-04-04 14:14:25 +0300

    Макет окна

--- MailSender3/MailSender.Lib/Entityes/Recipient.cs
+++ MailSender3/MailSender.Lib/Entityes/Recipient.cs
@@ -1,4 +1,9 @@
 ﻿using MailSender.Lib.Entityes.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MailSender.Lib.Entityes
 {
--- MailSender3/MailSender.Lib/Entityes/Server.cs
+++ MailSender3/MailSender.Lib/Entityes/Server.cs
@@ -1,4 +1,10 @@
-﻿namespace MailSender.Lib.Entityes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Lib.Entityes
 {
     public class Server
     {
