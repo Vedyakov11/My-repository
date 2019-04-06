22b2d38a8479c55ba40652008a6dd2f7b7ca0efc
Vedyakov11 <vedyakov11@gmail.com>
2019-04-06 06:32:54 +0300

    Добавили MVVM и сделали пример

---
+++ MailSender3/MailSender.WPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.WPF.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title = "Рассыльщик почты";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _Status = "Готов.";
+
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        //private double _Progress = 0.3;
+
+        //public double Progress
+        //{
+        //    get => _Progress;
+        //    set => Set(ref _Progress, value);
+        //}
+    }
+}
