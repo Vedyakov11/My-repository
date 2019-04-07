914af5d626b88feef43d812b65581beb636e9ada
Vedyakov11 <vedyakov11@gmail.com>
2019-04-07 18:42:17 +0300

    Фиксация перед MVVM.

--- MailSender/Data/TestData.cs
+++ MailSender/Data/TestData.cs
@@ -1,4 +1,5 @@
 ﻿using MailSender.Entityes;
+using MailSender.Servise;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,16 @@ namespace MailSender.Data
     {
         public static List<Server> Servers { get; } = new List<Server>
         {
-            new Server { Name = "Яндекс", Address = "smtp.yandex.ru", UserName = "UserName", Password = "Password" },
-            new Server { Name = "Mail.ru", Address = "smtp.mail.ru", UserName = "UserName", Password = "Password" },
-            new Server { Name = "gmail.com", Address = "smtp.gmail.com", UserName = "UserName", Password = "Password" }
+            new Server { Name = "Яндекс", Address = "smtp.yandex.ru", UserName = "UserName", Password = Encryptor.Encrypt("Password 1", 3) },
+            new Server { Name = "Mail.ru", Address = "smtp.mail.ru", UserName = "UserName", Password = Encryptor.Encrypt("Password 2", 3) },
+            new Server { Name = "gmail.com", Address = "smtp.gmail.com", UserName = "UserName", Password = Encryptor.Encrypt("Password 3", 3) }
         };
 
         public static List<Sender> Senders { get; } = new List<Sender>
         {
-            new Sender{ Name = "Иванов", EmailAddress ="ivanov@yandex.ru"},
-            new Sender{ Name = "Петров", EmailAddress ="ivanov@yandex.ru"},
-            new Sender{ Name = "Сидоров", EmailAddress ="ivanov@yandex.ru"}
+            new Sender{ Name = "Иванов", EmailAddress ="ivanov@yandex.ru" },
+            new Sender{ Name = "Петров", EmailAddress ="ivanov@yandex.ru" },
+            new Sender{ Name = "Сидоров", EmailAddress ="ivanov@yandex.ru" }
         };
 
     }
---
+++ MailSender/Servise/Encryptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Servise
+{
+    public static class Encryptor
+    {
+        public static string Encrypt(string str, int key)
+        {
+            return new string(str.Select(c => (char)(c + key)).ToArray());
+        }
+
+
+        public static string Decrypt(string str, int key)
+        {
+            return new string(str.Select(c => (char)(c - key)).ToArray());
+        }
+    }        
+}
